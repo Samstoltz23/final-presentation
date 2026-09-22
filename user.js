ce99690d1c968597089ac2ee5e6592e84e3921ca
Samantha Stoltzfus <samantha.stoltzfus23@gmail.com>
2026-09-22 16:37:35 -0400

    removed console.log(postsData)

--- user.js
+++ user.js
@@ -4,7 +4,6 @@ async function onSearchChange(event){
   const id = event.target.value;
   const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
   const postsData = await posts.json();
-  console.log(postsData)
   postListEl.innerHTML = postsData.map(post => `<div class="post">
       <div class="post__title">
         ${post.title}
