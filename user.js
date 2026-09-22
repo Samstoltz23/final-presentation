e8c40c7c9ff32ba88d28ff5bfbb05cd3762c0a0c
Samantha Stoltzfus <samantha.stoltzfus23@gmail.com>
2026-09-22 16:25:06 -0400

    console.log(postsData)

--- user.js
+++ user.js
@@ -4,6 +4,7 @@ async function onSearchChange(event){
   const id = event.target.value;
   const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
   const postsData = await posts.json();
+  console.log(postsData)
   postListEl.innerHTML = postsData.map(post => `<div class="post">
       <div class="post__title">
         ${post.title}
